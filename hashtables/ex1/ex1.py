37682bdf9fd6c14fd5223050c47dcbe989540991
Chris King <cking5836@gmail.com>
2020-10-02 13:05:59 -0400

    ex1 and 4 are done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,28 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
+    cache = {}
+    # Loop through weights
+    for index, weight in enumerate(weights):
+        # store result of limit - weight and index
+        cache[limit - weight] = index
+        # loop again
+    for index, i in enumerate(weights):
+        # Check to see if result is in cache
+        if i in cache:
+            # Is index greater than i?
+            if index > cache[i]:
+                results = (index, cache[i])
+                return results
+            else:
+                results = (cache[i], index)
+                return results
+        else:
+            continue
     return None
+
+
+# -> -> Time complexity should be O(n) <- <-
+
+weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+get_indices_of_item_weights(weights_4, 9, 7)
